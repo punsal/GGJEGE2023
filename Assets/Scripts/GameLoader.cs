609d369c55ecb71b438a23f0e7c16be6467ca0f0
Mustafa Pamir Ünsal <p_unsal@msn.com>
2023-02-04 04:40:27 +0300

    Remove old init files and add templates

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GameLoader : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //test
+        
     }
 
     // Update is called once per frame
